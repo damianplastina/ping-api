ee767e267ada4ea0113d7c0697a24ebf32de4df9
Damian Plastina <dplastina@auronix.com>
2024-06-23 14:43:02 -0300

    para saber desde qué ip estoy respondiendo

--- Controllers/PingController.cs
+++ Controllers/PingController.cs
@@ -1,3 +1,5 @@
+using System.Net;
+using System.Net.Sockets;
 using Microsoft.AspNetCore.Mvc;
 
 namespace PingApi.Controllers;
@@ -7,15 +9,32 @@ namespace PingApi.Controllers;
 public class PingController : ControllerBase
 {
     private readonly ILogger<PingController> _logger;
+    private string _localIpAddress;
 
     public PingController(ILogger<PingController> logger)
     {
         _logger = logger;
+        _localIpAddress = GetLocalIPAddress();
     }
 
     [HttpGet(Name = "ping")]
-    public String Get()
+    public ActionResult<string> Get()
     {
-        return "pong";
+        string respuesta = $"pong - Server IP: {_localIpAddress}";
+        _logger.LogInformation(respuesta);
+        return respuesta;
+    }
+    
+    private string GetLocalIPAddress()
+    {
+        var host = Dns.GetHostEntry(Dns.GetHostName());
+        foreach (var ip in host.AddressList)
+        {
+            if (ip.AddressFamily == AddressFamily.InterNetwork)
+            {
+                return ip.ToString();
+            }
+        }
+        throw new Exception("No network adapters with an IPv4 address in the system!");
     }
 }
